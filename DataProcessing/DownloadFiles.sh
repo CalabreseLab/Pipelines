498131ec7e920084b5d012a4be433fd7e1a57a9b
Sherry <wxueyao@unc.edu>
2025-01-08 17:31:22 -0500

    Update DownloadFiles.sh

--- DataProcessing/DownloadFiles.sh
+++ DataProcessing/DownloadFiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Replace the <start number> and <end number>
-acc= <your longleaf account>
-longleaf_dir= <your longleaf directory for abundance.tsv data>
-local_dir= <your local directory for downloading>
+# Replace <Start Number> and <End Number> with your own SRR number
+# Replace <Your Account> with your account
+# Replace <Your Online Path> with your online workplace directory and <Your Local Path> with your local directory.
 
-for i in $(seq <start number> <end number>); do
-  scp $acc: $longleaf_dir/SRR${i}/Kallisto_alignments/SRR${i}_abundance.tsv $local_dir
+for ((i=<Start Number>; i<=<End Number>; i++)); do
+  echo "Processing SRR${i}"  # Debugging output
+  scp <Your Account>:<Your Online Path>/SRR${i}/Kallisto_alignments/SRR${i}_abundance.tsv <Your Local Path>
 done
